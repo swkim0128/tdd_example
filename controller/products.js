ae78d765ed59159170f19308f90836fea8e14039
eunsol <swkim0128@github.com>
2021-02-06 02:42:20 +0900

    TST: update 기능 테스트 추가
    update 관련 유닛 테스트.
    에러 메세지 관련, 404 에러, 응답 테스트 추가
    및 update controller function 추가

--- controller/products.js
+++ controller/products.js
@@ -33,5 +33,19 @@ exports.getProductsById = async (req, res, next) => {
 }
 
 exports.updateProduct = async (req, res, next) => {
-
+    try {
+        const updatedProduct = await productModel.findByIdAndUpdate(
+            req.params.productId,
+            req.body,
+            { new: true }
+        )
+        
+        if(updatedProduct) {
+            res.status(200).json(updatedProduct);
+        } else {
+            res.status(404).send();
+        }
+    } catch (error) {
+        next(error);
+    }
 }
